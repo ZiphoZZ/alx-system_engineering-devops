6c15bb917e291645d5cc75b8db97eadff49161aa
ZiphoZZ <nkunkuzazz@gmail.com>
2024-03-22 20:13:08 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#install a package
-package { 'puppet-lint':
-    ensure   => '2.2.2',
-    provider => 'gem',
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
